52aefca77dfc4b688cd458609778cb52ad5a3de6
roctbb <roctbb@gmail.com>
2017-05-23 19:35:27 +0300

    add lesson 6

--- Lesson 6/images_bot.py
+++ Lesson 6/images_bot.py
@@ -13,19 +13,23 @@ def process(filename):
 
 @bot.message_handler(content_types=["photo"])
 def save(message):
+    #скачивание файла
     file_id = message.photo[-1].file_id
-
     path = bot.get_file(file_id)
     downloaded_file = bot.download_file(path.file_path)
 
+    #узнаешь расширение и придумываем имя
     extn = '.' + str(path.file_path).split('.')[-1]
-
     cname = str(uuid.uuid4()) + extn
+
+    #создаем файл и записываем туда данные
     with open('images/' + cname, 'wb') as new_file:
         new_file.write(downloaded_file)
 
+    #применяем фильтр
     process('images/' + cname)
 
+    #открываем файл и отправляем его пользователю
     with open('images/' + cname, 'rb') as new_file:
         bot.send_photo(message.chat.id, new_file.read())
 
