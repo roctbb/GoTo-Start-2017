716048487f7496b2517ac49818270437d445658c
roctbb <roctbb@gmail.com>
2017-06-03 15:08:49 +0300

    add lesson 10

--- Lesson 10/vk_api_test.py
+++ Lesson 10/vk_api_test.py
@@ -1,23 +1,24 @@
 import requests
 
-
+#формируем адрес страницы
 url = "https://api.vk.com/method/{0}?{1}&access_token={2}&v={3}"
 method = "wall.get"
 token = "5481ac075481ac075481ac073a54ddd87e554815481ac070db33118649cb6131da032cb"
 v = "5.65"
-params = "domain=goto_msk&count=100&offset={0}"
-
+params = "domain=roctbb&count=100&offset={0}"
 full_url = url.format(method, params.format(0),token,v)
+
+#делаем запрос
 result = requests.get(full_url)
 
+#узнаем количество записей
 json_result = result.json()
-
 response = json_result["response"]
-items = response["items"]
 count = response["count"]
 
 print(count)
 
+#теперь скачиваем все посты
 posts = []
 
 for offset in range(0, count, 100):
@@ -28,5 +29,9 @@ for offset in range(0, count, 100):
     items = response["items"]
     posts += items
 
-for post in posts:
+sorted_posts = sorted(posts, key=lambda x:x["likes"]["count"], reverse=True)
+
+#выводим
+for post in sorted_posts:
+    print(post["likes"]["count"])
     print(post["text"])
