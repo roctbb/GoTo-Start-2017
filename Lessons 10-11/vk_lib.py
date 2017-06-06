2f07bd868b9886a13103db69cd6d8f82fc82e662
roctbb <roctbb@gmail.com>
2017-06-06 18:30:46 +0300

    add lesson 11

---
+++ Lessons 10-11/vk_lib.py
@@ -0,0 +1,17 @@
+import time
+import vk
+
+token = "959baace8aefa7c8278304ad2d96da52cf98140773f24cca290d157288ae662e3b3375f14d26b1fa75450"
+
+session = vk.Session(access_token=token)
+api = vk.API(session)
+last_message_id = 0
+while True:
+    messages = api.messages.get(count=100, v=5.65, last_message_id=last_message_id)
+    if messages["items"]!=[]:
+        last_message_id = messages["items"][0]["id"]
+    for message in messages["items"]:
+        api.messages.send(user_id=message['user_id'], message='сам такой!', forward_messages=str(message['id']))
+    time.sleep(10)
+
+    print(messages)
